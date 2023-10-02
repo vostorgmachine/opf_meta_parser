f96decff1e53dc308a0166ca72e01dae03630be4
vostorgmachine <leonidvostok666@gmail.com>
2023-10-02 20:30:12 +0600

    -

--- main.py
+++ main.py
@@ -1,147 +1,139 @@
-#!/bin/python
+# !/bin/python3
 
-import glob
 from Markdown2docx import Markdown2docx
-from pathlib import Path
-import os
-import subprocess
+import glob 
 import markdown
 import re
+import os
 
-md_directory = glob.glob("*.md")
+md_files = glob.glob("*.md")
 
-n = 0
+search_dict= {"title":"## ", "authors":"Автор(ы): ", "seriya":"Серия: ", \
+        "publication_date":"Дата публикации статьи: ", "tags":"Теги: "}
 
-while n < len(md_directory):
+def find(filename, search_dict): 
+    """функция для поиска метаданных по словарю."""
 
-    md_file = open(md_directory[n], "r", encoding="utf-8")
+    result = {}
 
-    for md_string in md_file:
+    # Формирование двух списков. 
+    # Один содержит ключи, другой - их значения.
 
-        if "##" in md_string:
-            title = md_string
-            title = title.strip("## ").strip("\n")
-            md_file.seek(0)
-            break
+    search_list_keys = []
+    for i in search_dict.keys():
+        search_list_keys.append(i)
 
-    for md_string in md_file:
+    search_list_values = []
+    for i in search_dict.values():
+        search_list_values.append(i)
 
-        if "Серия:" in md_string:
-            seriya = md_string
-            seriya = seriya.strip("Серия: ").replace("№", "No: ")
+    with open(filename) as file:
+        result["file_name"] = filename
 
-            # Данная часть кода работает засчёт модуля re
-            # и на выходе даёт переменную nums, которая содержит номер серии,
-            # выраженный отдельным числом:
-            seriya_num = seriya
-            nums = re.findall(r"\d+", seriya_num)
-            nums = [int(i) for i in nums]
-            seriya_num = nums
-            md_file.seek(0)
-            break
 
-    for md_string in md_file:
+        # Переписать код таким образом, чтобы поиск шёл по ограниченному
+        # количеству строк (т.е только по самомоу началу файла.)
+        for string in file:
+            n = 0
+            while n < len(search_list_keys):
 
-        if "Автор(ы):" in md_string:
-            authors = md_string.replace("Автор(ы): ", "")
-            md_file.seek(0)
-            break
+                if string.startswith(search_list_values[n]):
 
-        else:
-            authors = "Неизвестный"
-            md_file.seek(0)
-            break
+                    result[search_list_keys[n]] =\
+                    string.strip(search_dict[search_list_keys[n]]).strip("\n")
+                n = n + 1
 
-        # В данном случае происходит забор строки, далее она "очищается" от
-        # ненужных символов, после чего конвертируется в список, который, в
-        # свою очередь переворачивается задом на перёд. В конце-концов
-        # создаётся строка, разделителем в которой является символ "-".
+    # В случае, если "авторов" в статье не окажется, данная строка 
+    # объявит автора неизвестным.
 
-    for md_string in md_file:
+    result['authors'] = 'Неизвестный' if result.get('authors') \
+            is None else result['authors']+''
+    
+    result['authors'] = result['authors'].split(" & ")
 
-        if "Дата публикации статьи:" in md_string:
-            date = md_string
-            date = (
-                date.strip("Дата публикации статьи: ")
-                .strip("\n")
-                .strip(" ")
-            )
-            date = date.split(".")[::-1]
-            date = "-".join(date)
-            md_file.seek(0)
-            break
+    file.close()
+    return result
 
-    for md_string in md_file:
-        if "Теги: " in md_string:
-            tags = md_string
-            tags = tags.strip("Теги: ")
-            md_file.seek(0)
-            break
+def displayer(file_list): # Функция для дебага
 
-    # --------------------------------------------------
-    # maker
-    # --------------------------------------------------
+    n = 0
+    print("--------------------------------------------------")
+    print("total amount of files: ", len(file_list))
+    while n < len(file_list):
 
-    # В данном случае задействован метод split(), позволяющий трансформировать
-    # строку в список. В качестве "разделителя" указан "&":
+        result = find(file_list[n], search_dict)
+        print("--------------------------------------------------")
+        for i in result:
+            print(i + ": ", result[i], '\n')
+        n = n + 1
+        result = {}
 
-    authors_list = authors.split("&")
+def author_reverser(author_list): # функция обработки авторов
 
-    second_authors_list = []
-    for i in authors_list:
-        i = i.strip()
-        second_authors_list.append(i)
-    authors_list = second_authors_list
+    reversed_author_list = []
+    reversed_author_string = ''
 
-    # Цикл для дробления элементов списка на отдельные слова (в данном случае имена
-    # и фамилии а так же их "переворачивание([::-1])".
+    if "Неизвестный" not in author_list:
 
-    authors_reversed = []
-    for each_author in authors_list:
-        splitted_author = each_author.split()
-        reversed_author = splitted_author[-1] + ", " + splitted_author[0]
-        authors_reversed.append(reversed_author)
+        for i in author_list:
+            i = i.split()
+            reversed_author = i[-1] + ", " + i[0]
+            reversed_author_list.append(reversed_author)
+            reversed_author_string = ' &amp; '.join(reversed_author_list)
 
-    opf_file_name = md_file.name.replace("md/", "").replace(".md", ".opf")
+    else:
+        return "Неизвестный"
 
-    opf_file = open(("./" + opf_file_name), "w+")
+    return reversed_author_string
+
+def author_link_map(author_list):
+
+    start = '<meta name="calibre:author_link_map" content="{'
+    end = '}"/>'
+    middle = ''
+
+    n = 0
+    while n < len(author_list):
+        middle = middle + "&quot;" + author_list[n]+' &quot;: &quot;&quot;,'
+        n += 1
+
+    final = start, middle, end
+    return str(final)
+
+def make_opf_file(result_dict):
+
+    opf_file_name = result_dict["file_name"].replace(".md", ".opf")
+    opf_file = open((opf_file_name), "w+")
+
     opf_head = open("/home/vostorg/sandbox/python/opf_meta_parser/head.opf", "r")
-
-    # Добавление "шапки"
     opf_file.write(opf_head.read())
-    opf_head.close()
 
-    opf_file.write("<dc:title>" + title + "</dc:title>")
-    opf_file.write("\n")
-    opf_file.write("\n")
-    # opf_file.close()
+    opf_file.write("<dc:title>" + result_dict["title"] + "</dc:title>\n\n")
 
-    # Данный цикл позволяет забрать список авторов и изменить разделитель его
-    # элементов на необходимый : (' &amp; '.join(authors_list)) - &amp в данном
-    # случае как раз и является указываемым делителем.
+    n = 0
 
-    i = 0
+    author_list = result_dict["authors"]
 
-    if authors != "":
+    first_section = '<dc:creator opf:file-as="' + author_reverser(author_list) +  '" opf:role="aut">'
 
-        while i < len(authors_list):
-            opf_file.write(
-                '<dc:creator opf:file-as="'
-                + (" &amp; ".join(authors_reversed))
-                + '" opf:role="aut">'
-                + authors_list[i]
-                + "</dc:creator>\n"
-            )
-            i = i + 1
+    # print(type(first_section))
 
-    contribution = open("/home/vostorg/sandbox/python/opf_meta_parser/contribution.opf", "r")
-    opf_file.write(contribution.read())
-    contribution.close()
+    if "Неизвестный" not in author_list:
 
-    opf_file.write("<dc:date>" + date + "T00:00:00+00:00</dc:date>\n")
-    opf_file.write("\n")
+        while n < len(author_list):
+            opf_file.write(first_section)
+            opf_file.write(author_list[n] + "</dc:creator>\n\n")
+            n = n + 1
+    else:
+            opf_file.write(first_section)
+            # hardcode!(4r
+            opf_file.write("Неизвестный")
+            opf_file.write("</dc:creator>\n\n")
 
-    with open(md_file.name, "r") as f:
+    opf_file.write("<dc:date>" + result_dict["publication_date"] +\
+                   "T00:00:00+00:00</dc:date>\n\n")
+
+    with open(result_dict['file_name'], "r") as f:
         markdown_file = f.read()
 
     html_file = markdown.markdown(markdown_file)
@@ -150,119 +142,66 @@ while n < len(md_directory):
     opf_file.write("\n")
 
     # Добавление тегов:
-    tags_list = tags.split(", ")
+    tags_list = result_dict['tags'].split(", ")
     for i in tags_list:
         i = i.strip("\n")
-        opf_file.write("<dc:subject>" + i + "</dc:subject>\n")
+        opf_file.write("<dc:subject>" + i + "</dc:subject>\n\n")
 
     opf_file.write("\n")
 
-    # А вот из этого ужаса нужно однозначно делать функцию.
+    opf_file.write(str(author_link_map(author_list)))
 
-    if authors == "Неизвестный":
-        opf_file.write(
-            ' <meta name="calibre:author_link_map"\
-    content="{&quot;'
-            + "Неизвестный"
-            + '&quot;: &quot;&quot;}"/> '
-        )
+    if "Ведомости" in result_dict['seriya']:
 
+        nums = re.findall(r"\d+", result_dict['seriya'])
+        seriya_num = [int(i) for i in nums]
+
+        opf_file.write('<meta name="calibre:series" content="' +\
+                "Ведомости" + '"/>\n\n')
+
+        opf_file.write('<meta name="calibre:series_index" content="' +\
+                str(seriya_num) + '"/>\n\n') 
+    
     else:
-        if authors != "":
 
-            if len(authors_list) == 1 and authors != "Неизвестный" :
-                opf_file.write(
-                    ' <meta name="calibre:author_link_map"\
-            content="{&quot;'
-                    + authors_list[0]
-                    + '&quot;: &quot;&quot;}"/> '
-                )
+        opf_file.write('<meta name="calibre:series" content="' +\
+                result_dict['seriya']+ '"/>\n\n')
 
-            if len(authors_list) == 2:
-                opf_file.write(
-                    '<meta name="calibre:author_link_map" content=\
-            "{&quot;'
-                    + authors_list[0]
-                    + "&quot;: &quot;&quot;, \
-            &quot;"
-                    + authors_list[1]
-                    + '&quot;: &quot;&quot;}"/>'
-                )
+    opf_file.write('<meta name="calibre:title_sort" content="' +
+                   result_dict['title'] + '"/>\n\n')
 
-            # Добавление author link. Обязательно переделать на что-то более пристойное!
-            if len(authors_list) == 3:
-                opf_file.write(
-                    ' <meta name="calibre:author_link_map" content="{&quot;'
-                    + authors_list[0]
-                    + "&quot;: &quot;&quot;, &quot;"
-                    + authors_list[1]
-                    + "&quot;: &quot;&quot;, &quot;"
-                    + authors_list[2]
-                    + ' &quot;: &quot;&quot;}"/>\n'
-                )
-
-            if len(authors_list) == 4:
-                opf_file.write(
-                    ' <meta name="calibre:author_link_map" content="{&quot;'
-                    + authors_list[0]
-                    + "&quot;: &quot;&quot;, &quot;"
-                    + authors_list[1]
-                    + "&quot;: &quot;&quot;, &quot;"
-                    + authors_list[2]
-                    + "&quot;: &quot;&quot;, &quot;"
-                    + authors_list[3]
-                    + ' &quot;: &quot;&quot;}"/>\n'
-                )
-
-            opf_file.write("\n")
-
-    if "Ведомости" in seriya:
-        seriya = "Ведомости"
-
-    if seriya[-1] == "\n":
-        seriya = seriya[:-1]
-
-    opf_file.write('<meta name="calibre:series" content="' + seriya + '"/>\n\n')
-
-    if seriya_num != "":
-        opf_file.write(
-            '<meta name="calibre:series_index" content="' + str(*seriya_num) + '"/>\n\n'
-        )
-
-    # Сделать временной штамп динамичным?
-    opf_file.write(
-        '<meta name="calibre:timestamp"\
+    opf_file.write( '<meta name="calibre:timestamp"\
      content="'
-        + date
-        + 'T00:00:00+00:00"/>\n'
+        + result_dict['publication_date']
+        + 'T00:00:00+00:00"/>\n\n'
     )
-    opf_file.write("\n")
-
-    if title[-1] == "\n":
-        title = title[:-1]
-    opf_file.write('<meta name="calibre:title_sort" content="' + title + '"/>\n')
-
-    # Финиш
-
     opf_file.write("\n\n</metadata>\n<guide/>\n</package>")
 
-    n = n + 1
+    opf_file.close()
 
-md_file.close()
-opf_file.close()
+def create_docx():
+    '''
+    Генератор docx файлов, который работает засчёт Markdown2docx-библиотеки
+    '''
+
+    directory = "./"
+
+    md_files_list = []
+    for file in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, file)) and file.endswith(".md"):
+            md_files_list.append(file)
 
 
-# Секция, отвечающая за конвертацию .md в .docx:
+    for file in md_files_list:
+        project = Markdown2docx(directory + file.strip(".md"))
+        project.eat_soup()
+        project.save()
 
-directory = "./"
+# main--------------------------------------------------
 
-md_files_list = []
-for file in os.listdir(directory):
-    if os.path.isfile(os.path.join(directory, file)) and file.endswith(".md"):
-        md_files_list.append(file)
+counter = 0
+while counter < len(md_files):
+    make_opf_file(find(md_files[counter], search_dict))
+    counter += 1
 
-
-for file in md_files_list:
-    project = Markdown2docx(directory + file.strip(".md"))
-    project.eat_soup()
-    project.save()
+create_docx()
